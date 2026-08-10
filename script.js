9e8fa80a92a1ccfac5e685563204275f312bb627
Sakuma Iyo <109006844+yosaguni@users.noreply.github.com>
2026-08-10 19:40:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// カードを順番に表示
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach((entry)=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.style.opacity="1";
+            entry.target.style.transform="translateY(0)";
+
+        }
+
+    });
+
+});
+
+cards.forEach((card)=>{
+
+    card.style.opacity="0";
+    card.style.transform="translateY(40px)";
+    card.style.transition="0.6s";
+
+    observer.observe(card);
+
+});
